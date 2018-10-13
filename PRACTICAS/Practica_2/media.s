7b043fedae9c3fd0621f88cdb0f08f073b47ad13
Antonio David Villegas Yeguas <advy99@gmail.com>
2018-10-13 15:57:58 +0200

    acabado 5.5 practica 2

--- PRACTICAS/Practica_2/media.s
+++ PRACTICAS/Practica_2/media.s
@@ -1,47 +1,55 @@
 .section .data
 #ifndef TEST
-#define TEST 19
+#define TEST 20
 #endif
 
 	.macro linea
-	#if   TEST==1
-	 	.int -1,-1,-1,-1
+	  #if   TEST==1
+	 	.int 1, 2, 1, 2
 	#elif TEST==2
-		.int 0x04000000, 0x04000000, 0x04000000, 0x04000000
+		.int -1, -2, -1, -2
 	#elif TEST==3
-		.int 0x08000000, 0x08000000, 0x08000000, 0x08000000  
+		.int 0x7fffffff, 0x7fffffff, 0x7fffffff, 0x7fffffff  
 	#elif TEST==4
-		.int 0x10000000, 0x10000000, 0x10000000, 0x10000000
-	#elif TEST==5
-		.int 0x7fffffff, 0x7fffffff, 0x7fffffff, 0x7fffffff
-	#elif TEST==6
 		.int 0x80000000, 0x80000000, 0x80000000, 0x80000000
-	#elif TEST==7
-		.int 0xf0000000, 0xf0000000, 0xf0000000, 0xf0000000
-	#elif TEST==8
-		.int 0xf8000000, 0xf8000000, 0xf8000000, 0xf8000000
-	#elif TEST==9
-		.int 0xf7ffffff, 0xf7ffffff, 0xf7ffffff, 0xf7ffffff
-	#elif TEST==10
-		.int 100000000, 100000000, 100000000, 100000000
-	#elif TEST==11
-		.int 200000000, 200000000, 200000000, 200000000
-	#elif TEST==12
-		.int 300000000, 300000000, 300000000, 300000000
-	#elif TEST==13
+	#elif TEST==5
+		.int 0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff
+	#elif TEST==6
 		.int 2000000000, 2000000000, 2000000000, 2000000000
-	#elif TEST==14
+	#elif TEST==7
 		.int 3000000000, 3000000000, 3000000000, 3000000000
-	#elif TEST==15
-		.int -100000000, -100000000, -100000000, -100000000
-	#elif TEST==16
-		.int -200000000, -200000000, -200000000, -200000000
-	#elif TEST==17
-		.int -300000000, -300000000, -300000000, -300000000
-	#elif TEST==18
+	#elif TEST==8
 		.int -2000000000, -2000000000, -2000000000, -2000000000
-	#elif TEST==19
+	#elif TEST==9
 		.int -3000000000, -3000000000, -3000000000, -3000000000
+	#endif
+		.endm
+
+		.macro linea0
+	
+	  #if TEST>=1 && TEST<=9
+		linea
+
+	#elif TEST==10
+		.int 0, 2, 1, 1
+	#elif TEST==11
+		.int 1, 2, 1, 1
+	#elif TEST==12
+		.int 8, 2, 1, 1
+	#elif TEST==13
+		.int 15, 2, 1, 1
+	#elif TEST==14
+		.int 16, 2, 1, 1
+	#elif TEST==15
+		.int 0, -2, -1, -1
+	#elif TEST==16
+		.int -1, -2, -1, -1
+	#elif TEST==17
+		.int -8, -2, -1, -1
+	#elif TEST==18
+		.int -15, -2, -1, -1
+	#elif TEST==19
+		.int -16, -2, -1, -1
 
 
 
@@ -50,15 +58,16 @@
 	#endif
 		.endm
 
-lista:		.irpc i, 1234
+lista:      linea0
+				.irpc i, 1234
 						linea
 				.endr
 
 longlista:	.int   (.-lista)/4
-resultado:	.quad   0
-  formato: 	.ascii	"resultado \t =   %18ld (sgn)\n"
-				.ascii			  "\t\t = 0x%18lx (hex)\n"
-				.asciz			  "\t\t = 0x %08x %08x \n"
+media:		.int	  0
+resto: 		.int    0
+formato: 	.ascii	"media \t = %11d \t resto \t = %11d  \n"   
+				.asciz			"\t = 0x %08x \t    \t = %08x \n"
 
 # 4) gcc media.s -o media -no-pie
 
@@ -68,16 +77,20 @@ main: .global  main
 	mov     $lista, %rbx
 	mov  longlista, %ecx
 	call suma		# == suma(&lista, longlista);
-	mov  %eax, resultado    #primera seccion de la solucion
-	mov  %edx, resultado+4
+
+
+	call calc_media
+	mov %edx, resto
+	mov %eax, media
+
 
    #imprimir usando libC
 
 	mov   $formato, %rdi #primer parametro, el formato
-	mov   resultado,%rsi #segundo parametro, resultado en decimal
-	mov   resultado,%rdx #tercer parametro, resultado en hexadecimal
-	mov   resultado+4,%rcx #cuarto parametro, LSB en hexadecimal (recordar little-endian)
-	mov   resultado,%r8 #quito paremetro, MSB en hexadecimal 
+	mov   media,%rsi #segundo parametro, resultado en decimal
+	mov   resto,%rdx #tercer parametro, resultado en hexadecimal
+	mov   media,%rcx #cuarto parametro, LSB en hexadecimal (recordar little-endian)
+	mov   resto,%r8 #quito paremetro, MSB en hexadecimal 
 
 	mov          $0,%eax	# varargin sin xmm
 	call  printf		# == printf(formato, res, res);
@@ -103,7 +116,8 @@ suma:
 
 bucle:
 	mov  (%rbx,%rsi,4), %eax
-	cltd
+	cltd #extension de signo EDX:EAX <- EAX
+
 	add  %eax, %ebp
 
 	adc 	%edx, %edi
@@ -117,5 +131,12 @@ bucle:
 
 	pop %rdi
 	pop %rbp
+
+	ret
+
+
+calc_media:
+	idiv	 %ecx  # %edx <- RDX:RAX mod ECX
+				  # %eda <- RDX:RAX / ECX
 
 	ret
