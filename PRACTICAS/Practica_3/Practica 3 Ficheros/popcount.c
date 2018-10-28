e5c7ed1dfada56b2f1178eb9e44c2947d11f7fe7
Antonio David Villegas Yeguas <advy99@gmail.com>
2018-10-28 18:08:45 +0100

    todas las versiones del popcount

--- PRACTICAS/Practica_3/Practica 3 Ficheros/popcount.c
+++ PRACTICAS/Practica_3/Practica 3 Ficheros/popcount.c
@@ -268,6 +268,98 @@ int popcount7(unsigned* array, size_t len)
 
 
 
+int popcount8(unsigned * array, size_t len){
+	size_t i;
+	int val, res = 0;
+	int SSE_mask [] = {0x0f0f0f0f, 0x0f0f0f0f, 0x0f0f0f0f, 0x0f0f0f0f};
+	int SSE_LUTb [] = {0x02010100, 0x03020201, 0x03020201, 0x04030302}; 
+
+	if (len & 0x3) printf("letendo 128b pero len no multiplo de 4\n");
+
+	for (i = 0; i < len; i+=4){
+		asm(  "movdqu    %[x], %%xmm0  \n\t" 
+				"movdqa  %%xmm0, %%xmm1  \n\t"  //  x: two copies xmm0-1 
+				"movdqu    %[m], %%xmm6  \n\t"  //  mask: xmm6 
+				"psrlw      $4 , %%xmm1  \n\t" 
+				"pand    %%xmm6, %%xmm0  \n\t"  //; xmm0 – lower  nibbles 
+				"pand    %%xmm6, %%xmm1  \n\t"  //; xmm1 – higher nibbles
+
+				"movdqu    %[l], %%xmm2  \n\t"  //; since instruction pshufb modifies LUT 
+				"movdqa  %%xmm2, %%xmm3  \n\t"  //; we need 2 copies 
+				"pshufb  %%xmm0, %%xmm2  \n\t"  //; xmm2 = vector of popcount lower nibbles 
+				"pshufb  %%xmm1, %%xmm3  \n\t"  //; xmm3 = vector of popcount upper nibbles 
+				
+				"paddb   %%xmm2, %%xmm3  \n\t"  //; xmm3 – vector of popcount for bytes 
+				"pxor    %%xmm0, %%xmm0  \n\t"  //; xmm0 =  0,0,0,0 
+				"psadbw  %%xmm0, %%xmm3  \n\t"  //; xmm3 = [pcnt bytes0..7|pcnt bytes8..15] 
+				"movhlps %%xmm3, %%xmm0  \n\t"  //; xmm0 = [       0      |pcnt bytes0..7 ] 
+				"paddd   %%xmm3, %%xmm0  \n\t"  //; xmm0 = [  not needed  |pcnt bytes0..15]
+				"movd    %%xmm0, %[val]      "
+				: [val] "=r" (val)
+				:  [x]   "m" (array[i]), 
+					[m]   "m" (SSE_mask[0]), 
+					[l]   "m" (SSE_LUTb[0]) 
+		
+		);
+
+		res += val;
+	}
+
+	return res;
+
+}
+
+
+int popcount9(unsigned* array, size_t len){ 
+   size_t i; 
+   unsigned x; 
+   int val, res=0; 
+   
+	for (i=0; i<len; i++){
+   	x = array[i]; 
+      
+		asm("popcnt %[x], %[val]"
+			: [val] "=r" (val) 
+			:   [x]  "r" (x) 
+      ); 
+  
+      res += val; 
+   
+	}
+   
+	return res; 
+}
+
+
+int popcount10(unsigned* array, size_t len){
+	size_t i;
+	unsigned long x1, x2;
+	long val; int res = 0;
+
+	if (len & 0x3) printf("leyendo 128b pero len no multiplo de 4\n");
+
+	for (i = 0; i < len; i+=4){
+		x1 = *(unsigned long*) &array[i];	
+		x2 = *(unsigned long*) &array[i + 2];
+		
+		asm("popcnt %[x1], %[val]  \n\t" //val vale 0, podemos contar ahi
+			 "popcnt %[x2], %[x1]  \n\t"	//x1 ya lo hemos contado, podemos pisarlo y contar ahi
+			 "add  %[x1] , %[val]  \n\t"	//sumamos los dos registros en la salida
+			: [val] "=&r" (val)
+			: [x1]    "r" (x1),
+			  [x2]    "r" (x2)
+		);
+
+		res += val;
+	}
+
+	return res;
+}
+
+
+
+
+
 
 
 
@@ -304,9 +396,9 @@ int main()
 	crono(popcount5 , "popcount5 (CS:APP2e 3.49-group 8b )");
 	crono(popcount6 , "popcount6 (Wikipedia- naive - 32b )");
 	crono(popcount7 , "popcount7 (Wikipedia- naive - 128 )");
-//	crono(popcount8 , "popcount8 (asm SSE3 - pshufb 128b )");
-//	crono(popcount9 , "popcount9 (asm SSE4- popcount 32b )");
-//	crono(popcount10, "popcount10 (asm SSE4- popcount 128)");
+	crono(popcount8 , "popcount8 (asm SSE3 - pshufb 128b )");
+	crono(popcount9 , "popcount9 (asm SSE4- popcount 32b )");
+	crono(popcount10, "popcount10 (asm SSE4- popcount 128)");
 
 #if TEST != 0
 	printf("calculado = %d\n", RESULT);
